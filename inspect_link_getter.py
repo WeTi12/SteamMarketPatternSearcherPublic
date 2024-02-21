c1dae00918ba75640ba5306b78ee01b3de8780c1
PSob888 <235975@edu.p.lodz.pl>
2024-02-21 12:02:24 +0100

    added more driver arguments to further reduce ram usage

--- inspect_link_getter.py
+++ inspect_link_getter.py
@@ -23,6 +23,12 @@ if type(data) == dict:
 #open browser
 options = Options() 
 options.add_argument("-headless")
+options.add_argument("-disable-infobars")
+options.add_argument("-disable-extensions")
+options.add_argument('-disable-application-cache')
+options.add_argument('-no-sandbox')
+options.add_argument('-disable-gpu')
+options.add_argument("-disable-dev-shm-usage")
 options.page_load_strategy = 'eager'
 options.set_preference("permissions.default.image", 2)
 driver = webdriver.Firefox(options)
